0bba221655ef89f0b03f8b2a16d52721e5f5bfd3
zbrill <c.hits@yahoo.com>
2025-04-26 13:18:01 -0700

    get headers from csv in gcs

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from typing import Dict
 import csv
 import pandas as pd
-from io import BytesIO
+from io import BytesIO, StringIO
 from google.cloud import storage
 
 def parse_target_format(tf: Dict):
@@ -45,7 +45,28 @@ def get_headers_from_gcs_excel(bucket_name, blob_name):
       excel_file = BytesIO(content)
 
       df = pd.read_excel(excel_file, nrows=1)
+
       return df.columns.tolist()
+
    except Exception as e:
       print(f"Error reading Excel file from GCS: {e}")
+      return None
+
+def get_headers_from_gcs_csv(bucket_name, blob_name):
+   try:
+      storage_client = storage.Client()
+
+      bucket = storage_client.get_bucket(bucket_name)
+      blob = bucket.blob(blob_name)
+
+      content = blob.download_as_string().decode('utf-8')
+      csv_file = StringIO(content)
+
+      csv_reader = csv.reader(csv_file)
+      headers = next(csv_reader)
+
+      return headers
+
+   except Exception as e:
+      print(f"Error reading CSV file from GCS: {e}")
       return None
